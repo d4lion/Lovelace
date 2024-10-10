b8ccef8300d73239c4ea823151fc95bd757edbf9
d4lion <d4lion.dev@gmail.com>
2024-10-09 19:00:25 -0500

    :sparkles: Database connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager
+                .getConnection("jdbc:postgresql://localhost:5432/Testing", "postgres", "d4lion" +
+                "#031");
+
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM personas")){
+            while (rs.next()) {
+                System.out.println(rs.getString("nombre") + " " + rs.getString("apellido"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
